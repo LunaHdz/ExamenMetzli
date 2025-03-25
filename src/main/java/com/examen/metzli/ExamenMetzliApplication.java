a0ae83d19c2125cd996c5caa3dc34955bcc00511
Metzli <metzliisc@gmail.com>
2025-03-25 14:17:07 -0600

    primer commit Metzli

---
+++ src/main/java/com/examen/metzli/ExamenMetzliApplication.java
@@ -0,0 +1,13 @@
+package com.examen.metzli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenMetzliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenMetzliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/examen/metzli/ExamenMetzliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examen.metzli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenMetzliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
